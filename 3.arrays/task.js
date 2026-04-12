5bc803043715e36ffb80b1aee2630c158200b421
Mariya Buslaeva <buslaeva.mariya11@gmail.com>
2026-04-12 17:13:35 +0400

    выполнено задание 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,25 @@
+"use strict";
+
+// Задача 1
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((item, index) => item === arr2[index]);
 }
 
+// Задача 2
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filtered = users.filter(user => user.gender === gender);
+
+  if (filtered.length === 0) {
+    return 0;
+  }
+
+  const totalAge = filtered
+    .map(user => user.age)
+    .reduce((sum, age) => sum + age, 0);
+
+  return totalAge / filtered.length;
 }
